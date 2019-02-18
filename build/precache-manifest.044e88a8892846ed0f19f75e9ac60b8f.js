e4529187a79111c912c13932387b1c0e88bdc888
Victor_Rodzko <vicrodzko@mail.ru>
2019-02-18 14:55:54 +0300

    add build version for production

---
+++ build/precache-manifest.044e88a8892846ed0f19f75e9ac60b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "64c90ea731958cd83b88",
+    "url": "/static/js/main.64c90ea7.chunk.js"
+  },
+  {
+    "revision": "a227f6f47fe8cdc6c185",
+    "url": "/static/js/2.a227f6f4.chunk.js"
+  },
+  {
+    "revision": "64c90ea731958cd83b88",
+    "url": "/static/css/main.0a04394e.chunk.css"
+  },
+  {
+    "revision": "923ac107e69cc8f2117fa48c89bfa882",
+    "url": "/index.html"
+  }
+];
